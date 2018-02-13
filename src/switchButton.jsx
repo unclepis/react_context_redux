f86fb53c856a23d69027d7a6eb7c10fb1e29ebf6
unclepis <59869158@qq.com>
2018-02-13 18:04:46 +0800

    Update switchButton.jsx

--- src/switchButton.jsx
+++ src/switchButton.jsx
@@ -1,33 +1,44 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ContextComponent from './highOrder'
+import { connect } from './react-redux' // 导入手写的高阶函数connect从context中获取store
+// import { connect } from 'react-redux' // 官方提供的connect高阶函数
 
-class SwitchButton extends Component {
+class ThemeSwitch extends Component {
     static contextTypes = {
-        store: PropTypes.object
+        themeColor: PropTypes.string,
+        onSwitchColor: PropTypes.func
     }
 
-    _changeThemeColor(color) {
-        const { store } = this.context;
-        store.dispatch({
-            type: 'change_color',
-            themeColor: color
-        })
+    handleChangeThemeColor(color) {
+        if (this.props.onSwitchColor) {
+            this.props.onSwitchColor(color);
+        }
     }
 
     render() {
-        const options = ['red', 'blue'];
         return (
             <div>
-                {
-                    options.map(option => {
-                        return <button style={{ color: this.props.data }} onClick={this._changeThemeColor.bind(this, option)}>{option}</button>
-                    })
-                }
+                <button style={{ color: this.props.themeColor }} onClick={this.handleChangeThemeColor.bind(this, 'red')}>red</button>
+                <button style={{ color: this.props.themeColor }} onClick={this.handleChangeThemeColor.bind(this, 'blue')}>blue</button>
             </div>
-        );
+        )
     }
 }
 
-export default SwitchButton = ContextComponent(SwitchButton);
+const mapDispatchToProps = (dispatch) => { // 用于需要获取用户的action，需要获取store中的dispatch，从mapDispatchToProps中获取
+    return {
+        onSwitchColor: (color) => {
+            dispatch({
+                type: 'change_color',
+                themeColor: color
+            });
+        }
+    }
+}
 
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+export default ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch);
