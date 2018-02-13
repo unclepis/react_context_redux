7cfe2fb7de7530ecb4b560bab91abadf44dc1191
unclepis <59869158@qq.com>
2018-02-13 18:01:19 +0800

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import ContextComponent from './highOrder'
+import { connect } from './react-redux' // 从手写的react-redux中引入connect高阶函数，把从context中获取store的通用逻辑封装到dumb组件Header
+import { connect } from 'react-redux'
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                <h1 style={{ color: this.props.data }}>React with redux and context!</h1>
-            </div>
+            <h1 style={{ color: this.props.themeColor }}>Redux和context和混合使用</h1>
         )
     }
 }
+const mapStateToProps = (state) => { // 根据Header这个无状态的pumb组件需要state中的上面参数，对高阶connect组件进行配置
+    return {
+        themeColor: state.themeColor
+    }
+}
 
-export default Header = ContextComponent(Header);
+export default Header = connect(mapStateToProps)(Header);
